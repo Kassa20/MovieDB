1b61dddcf0927658b295ebf02d2c7c4839a11ed7
neo <ykassa47@gmail.com>
2024-04-20 14:50:24 -0500

    movie API added

--- Front-end/app.js
+++ Front-end/app.js
@@ -1,3 +1,62 @@
+//fetch movies from MovieAPI
+
+const url = 'https://online-movie-database.p.rapidapi.com/title/v2/get-chart-rankings?rankingsChartType=TOP_250&first=100';
+let topMoviesCount = 20; 
+fetchMovies(topMoviesCount);
+
+function fetchMovies(count) {
+    fetch(url, {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '141d834db9msh6963c5ac2621dfdp121ed7jsn3784efe1e456',
+            'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        const list = data.data.titleChartRankings.edges.slice(0, count); 
+        console.log(list);
+        let rank = 1;
+
+        const tableBody = document.querySelector('.table-body');
+        tableBody.innerHTML = ''; 
+
+        list.forEach(item => {
+            const rating = item.node.chartRating.toFixed(1);
+            const title = item.node.item.originalTitleText.text;
+            const year = item.node.item.releaseYear.year;
+            const movie = `
+                <tr>
+                    <td>${rank}</td>
+                    <td>${title}</td>
+                    <td>${rating}</td>
+                    <td>${year}</td>
+                </tr>`;
+            tableBody.innerHTML += movie;
+            rank++;
+        });
+    })
+    .catch(err => {
+        console.log(err);
+    });
+}
+
+
+
+// Event listener for checkbox
+const checkbox = document.querySelector('.check-box');
+checkbox.addEventListener('change', function() {
+    if (this.checked) {
+        topMoviesCount = 100; 
+    } else {
+        topMoviesCount = 20; 
+    }
+    fetchMovies(topMoviesCount); 
+});
+
+
+
+
 const arrows = document.querySelectorAll(".arrow");
 const movieLists = document.querySelectorAll(".movie-list")
 
@@ -43,5 +102,4 @@ arrows.forEach((arrow, i) => {
 
     console.log(movieLists[i].querySelector("img").length)
 })
-
 
